573564c2e2369dbac28e74e8d318645e9f6a25f0
bma <zafer.bora.manga@reinmedical.com>
2022-01-16 17:02:19 +0100

    firebase config file added

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "vuejs-axios-blog-cd13c.firebaseapp.com",
+  databaseURL: "https://vuejs-axios-blog-cd13c.firebaseio.com",
+  projectId: "vuejs-axios-blog-cd13c",
+  storageBucket: "vuejs-axios-blog-cd13c.appspot.com",
+  messagingSenderId: "317743287767",
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
+  measurementId: "G-52JHE4RDKP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db=getFirestore()
