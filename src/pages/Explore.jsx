cfa46997e5313809cea0d2ac0d76dd4f25c1a1bf
bma <zafer.bora.manga@reinmedical.com>
2022-04-09 12:55:53 +0200

    - Adding route for category page, - Explore page added, - Spinner component added, - Category component added, firebase connected for listing.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 import React from 'react'
 
 function Explore() {
     return (
-        <div>
-            <h1>Explore</h1>
+        <div className='explore'>
+            <header>
+                <p className="pageHeader">Explore</p>
+            </header>
+            <main>
+                {/* Slider */}
+                <p className="exploreCategoryHeading">Categories</p>
+                <div className="exploreCategories">
+                    <Link to='/category/rent'>
+                        <img src={rentCategoryImage} alt="rent-category" className='exploreCategoryImg'/>
+                        <p className='exploreCategoryName'>Places For Rent</p>
+                    </Link>
+                    <Link to='/category/sale'>
+                        <img src={sellCategoryImage} alt="sell-category" className='exploreCategoryImg'/>
+                        <p className='exploreCategoryName'>Places For Sale</p>
+
+                    </Link>
+                </div>
+            </main>
         </div>
     )
 }
